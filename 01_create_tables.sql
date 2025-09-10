b5d367a5674c72546023254fdd4dead6ec40377d
Cruz <183213@upslp.edu.mx>
2025-09-10 00:03:10 -0600

    Update 01_create_tables.sql
    lo acomode para que agarre

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -24,8 +24,11 @@ CREATE TABLE grupo(
   id_grupo SERIAL PRIMARY KEY,
   clave_grupo VARCHAR(10) NOT NULL,
   id_materia INT NOT NULL,
-  FOREIGN KEY(id_materia) REFERENCES materia(id_materia),
   matricula_maestro VARCHAR(10) NOT NULL,
+  periodo VARCHAR(10) NOT NULL,       -- lee agrege este campo para que me acepte el 3ro xd
+  seccion NUMERIC(1) NOT NULL,        -- este tambien
+  nombre_grupo VARCHAR(50) NOT NULL,  -- y este
+  FOREIGN KEY(id_materia) REFERENCES materia(id_materia),
   FOREIGN KEY(matricula_maestro) REFERENCES maestro(matricula_maestro)
 );
 
@@ -33,10 +36,12 @@ CREATE TABLE asistencia(
   id_asistencia SERIAL PRIMARY KEY,
   fecha_asistencia TIMESTAMP NOT NULL,
   matricula_alumno VARCHAR(10) NOT NULL,
-  FOREIGN KEY(matricula_alumno) REFERENCES alumno(matricula_alumno),
   id_grupo INT NOT NULL,
-  FOREIGN KEY(id_grupo) REFERENCES grupo(id_grupo),
-  presente BOOLEAN NOT NULL
+  presente BOOLEAN NOT NULL,
+  periodo VARCHAR(10) NOT NULL,   -- este es nuevo tambien para que agarre
+  seccion NUMERIC(1) NOT NULL,    -- otro igual nuevo
+  FOREIGN KEY(matricula_alumno) REFERENCES alumno(matricula_alumno),
+  FOREIGN KEY(id_grupo) REFERENCES grupo(id_grupo)
 );
 
 CREATE TABLE inscripcion(
